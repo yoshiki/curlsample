1a0e06a85d66452fa013f021325925fcedfbd4e5
Yoshiki Kurihara <clouder@gmail.com>
2016-04-19 01:29:28 +0900

    add callback process

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# curlsample
+# curltest
 
 Sample usage of C bindings for the libcurl library in **Swift**.
+
+You need to install libcurl first.
+
+And compile it.
+
+```
+% swift build -Xcc -I/usr/local/include -Xlinker -L/usr/local/lib
+% .build/debug/curltest
+```
--- Sources/main.swift
+++ Sources/main.swift
@@ -2,15 +2,47 @@ import CCurl
 
 let handle = curl_easy_init()
 
+// set url
 let url = "https://www.example.com"
 url.withCString {
     curlHelperSetOptString(handle, CURLOPT_URL, UnsafeMutablePointer($0))
 }
+
+// set verbose
 curlHelperSetOptBool(handle, CURLOPT_VERBOSE, CURL_TRUE)
 
-let ret = curl_easy_perform(handle)
-let error = curl_easy_strerror(ret)
-print("error = \(error)")
-print("ret = \(ret)")
+// set timeout
+let timeout = 3
+curlHelperSetOptInt(handle, CURLOPT_TIMEOUT, timeout)
 
+// a class to store received data
+class Received {
+    var data = String()
+}
+
+// set write func
+var received = Received()
+curlHelperSetOptWriteFunc(handle, &received) { (buf: UnsafeMutablePointer<Int8>, size: Int, nMemb: Int, privateData: UnsafeMutablePointer<Void>) -> Int in
+    let p = UnsafePointer<Received?>(privateData)
+    if let line = String.fromCString(buf) {
+        p.memory?.data.appendContentsOf(line)
+    }
+    return size * nMemb
+}
+
+// perform
+let ret = curl_easy_perform(handle)
+
+// result handling
+if ret == CURLE_OK {
+    print(received.data)
+} else {
+    let error = curl_easy_strerror(ret)
+    if let errStr = String.fromCString(error) {
+        print("error = \(errStr)")
+    }
+    print("ret = \(ret)")
+}
+
+// cleanup
 curl_easy_cleanup(handle)
