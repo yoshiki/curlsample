e7ad5b001eb29e18aaf25bba9488238bb8771be4
Yoshiki Kurihara <clouder@gmail.com>
2016-04-19 18:14:26 +0900

    add post data

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,12 +1,23 @@
 import CCurl
 
+// a class to store received data
+class Received {
+    var data = String()
+}
+
+class Send {
+    let data = "{ \"foo\": [ \"bar\", \"baz\" ] }"
+}
+
 let handle = curl_easy_init()
 
 // set url
-let url = "https://www.example.com"
+let url = "http://www.example.com"
 url.withCString {
     curlHelperSetOptString(handle, CURLOPT_URL, UnsafeMutablePointer($0))
 }
+
+curlHelperSetOptInt(handle, CURLOPT_HTTP_TRANSFER_DECODING, 0)
 
 // set verbose
 curlHelperSetOptBool(handle, CURLOPT_VERBOSE, CURL_TRUE)
@@ -15,16 +26,47 @@ curlHelperSetOptBool(handle, CURLOPT_VERBOSE, CURL_TRUE)
 let timeout = 3
 curlHelperSetOptInt(handle, CURLOPT_TIMEOUT, timeout)
 
-// a class to store received data
-class Received {
-    var data = String()
+curlHelperSetOptBool(handle, CURLOPT_POST, CURL_TRUE)
+
+// set header
+var headersList: UnsafeMutablePointer<curl_slist> = nil
+var headers: [(String, String)] = [
+    // ("Content-Type", "text/plain"),
+    // ("Accept", "application/json"),
+    // ("Charset", "utf8"),
+]
+for (key, value) in headers {
+    let header = "\(key): \(value)"
+    header.withCString {
+        headersList = curl_slist_append(headersList, UnsafeMutablePointer($0))
+    }
 }
+if headersList != nil {
+    curlHelperSetOptHeaders(handle, headersList)
+}
+
+// set post fields
+var send = Send()
+send.data.withCString {
+    let data = UnsafeMutablePointer<Int8>($0)
+    curlHelperSetOptInt(handle, CURLOPT_POSTFIELDSIZE, Int(strlen(data)))
+}
+
+curlHelperSetOptReadFunc(handle, &send) { (buf: UnsafeMutablePointer<Int8>, size: Int, nMemb: Int, privateData: UnsafeMutablePointer<Void>) -> Int in
+    let p = UnsafePointer<Send?>(privateData)
+    let len = size * nMemb
+    if let data = p.memory?.data {
+        memcpy(buf, data, len)
+    }
+    return len
+}
+
 
 // set write func
 var received = Received()
-curlHelperSetOptWriteFunc(handle, &received) { (buf: UnsafeMutablePointer<Int8>, size: Int, nMemb: Int, privateData: UnsafeMutablePointer<Void>) -> Int in
+curlHelperSetOptWriteFunc(handle, &received) { (ptr: UnsafeMutablePointer<Int8>, size: Int, nMemb: Int, privateData: UnsafeMutablePointer<Void>) -> Int in
     let p = UnsafePointer<Received?>(privateData)
-    if let line = String.fromCString(buf) {
+    if let line = String.fromCString(ptr) {
         p.memory?.data.appendContentsOf(line)
     }
     return size * nMemb
@@ -46,3 +88,7 @@ if ret == CURLE_OK {
 
 // cleanup
 curl_easy_cleanup(handle)
+
+if headersList != nil {
+    curl_slist_free_all(headersList)
+}
